a3410f89ff58b8ef1d5f8ff920df0ed2582b762a
olhasliusar <olha.sliusar0315@gmail.com>
2016-03-20 21:52:49 +0200

    cake.html

---
+++ js/scroll-menu.js
@@ -0,0 +1,25 @@
+;(function($) {
+
+  var $menu = $("#ws-menu"),
+    hClass = "ws-menu__section_fixed",
+    pos = parseFloat( $(".ws-header").css("height") ) - 103;
+
+  $(document).scroll(function() {
+    if($(this).scrollTop() > pos) {
+        if (!$menu.hasClass(hClass)) {
+            $menu
+                .addClass("ws-menu__section_fixed");
+            $(".welcom-to-site__section")
+                .addClass("welcom-to-site__section_fixed");
+        }
+    } else {
+        if ($menu.hasClass(hClass)) {
+            $menu
+                .removeClass("ws-menu__section_fixed");
+            $(".welcom-to-site__section")
+                .removeClass("welcom-to-site__section_fixed");
+        }
+    }
+  });
+
+})(jQuery);
