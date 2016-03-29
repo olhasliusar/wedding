bc46a74f2110ecd25211344f50ce41dad78e6d40
olhasliusar <olha.sliusar0315@gmail.com>
2016-03-29 17:25:42 +0300

    music piano work

--- js/music.js
+++ js/music.js
@@ -1,11 +1,15 @@
 ;(function($) {
 
 	$(".ws-menu__video-piano").on("click", function(){
-	    var $videoPiano = $(".ws-video_piano"),
+	    setTimeout(calcVideoHeight, 10);
+	});
+
+	function calcVideoHeight(){
+    	var $videoPiano = $(".ws-video_piano"),
 	    	$wVideoPiano = $videoPiano.width();
 
 	    return $videoPiano.height( Math.round($wVideoPiano / 1.7) );
-	});
+	}
 
 })(jQuery);
 
