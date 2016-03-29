9491e0d17fd25e42549729aa7c9216511fd3fb6e
olhasliusar <olha.sliusar0315@gmail.com>
2016-03-29 17:19:04 +0300

    music piano js

--- js/music.js
+++ js/music.js
@@ -1,15 +1,11 @@
 ;(function($) {
 
 	$(".ws-menu__video-piano").on("click", function(){
-	    setTimeout(calcVideoHeight, 0.01);
-	});
-
-	function calcVideoHeight(){
-    	var $videoPiano = $(".ws-video_piano"),
+	    var $videoPiano = $(".ws-video_piano"),
 	    	$wVideoPiano = $videoPiano.width();
 
 	    return $videoPiano.height( Math.round($wVideoPiano / 1.7) );
-	}
+	});
 
 })(jQuery);
 
