ecada2137573b5c0a4e12d43671746c570fffd34
olhasliusar <olha.sliusar0315@gmail.com>
2016-03-30 22:54:42 +0300

    video all

--- js/music.js
+++ js/music.js
@@ -3,7 +3,7 @@
 	var $videoPiano = $(".ws-menu_music-dropdown").parent().find('.ws-video');
     	$wVideoPiano = $(".ws-menu__video").width();
 
-    $videoPiano.height( Math.round($wVideoPiano / 1.6) );
+    $videoPiano.height( Math.round($wVideoPiano / 1.8) );
 
 })(jQuery);
 
