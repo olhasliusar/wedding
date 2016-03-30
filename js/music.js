ce581e60a5f4faf46f3c49d46c793cf5d93d3b83
olhasliusar <olha.sliusar0315@gmail.com>
2016-03-30 22:53:24 +0300

    video all

--- js/music.js
+++ js/music.js
@@ -3,7 +3,7 @@
 	var $videoPiano = $(".ws-menu_music-dropdown").parent().find('.ws-video');
     	$wVideoPiano = $(".ws-menu__video").width();
 
-    $videoPiano.height( Math.round($wVideoPiano / 1.7) );
+    $videoPiano.height( Math.round($wVideoPiano / 1.6) );
 
 })(jQuery);
 
